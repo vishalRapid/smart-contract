4250a997f42692fd9e140eed2e08f59941cf5a2d
vishal <vishal@vishals-MacBook-Pro.local>
2022-11-03 12:02:43 +0530

    Refactoring code as per suggestions

--- LearningContracts/blockchain_test_erc721.sol
+++ LearningContracts/blockchain_test_erc721.sol
@@ -12,10 +12,12 @@ contract NFTTESTING is ERC721, Ownable {
     using Counters for Counters.Counter;
     address public tokenAddress;
     uint256 public rate = 100 * 10**18;
+    uint32 public timeInterval = 3600;
     Counters.Counter private _tokenIdCounter;
     struct UserLimit {
         uint8 count;
         uint32 timeLimit;
+        bool exists;
     }
     mapping(address => UserLimit) userLimit;
 
@@ -28,18 +30,30 @@ contract NFTTESTING is ERC721, Ownable {
 
     modifier checkUserLimit(address _to) {
         UserLimit memory currentUserStat = userLimit[_to];
-        if (currentUserStat.count < 5) {
-            currentUserStat.count++;
-        } else if (
-            currentUserStat.count == 5 &&
-            currentUserStat.timeLimit < block.timestamp
-        ) {
-            // check for timestamp
+
+        if (currentUserStat.exists == false) {
+            // checking if user already minted or not
             currentUserStat.count = 1;
-            currentUserStat.timeLimit = uint32(block.timestamp);
+            currentUserStat.timeLimit = uint32(block.timestamp + timeInterval);
+            currentUserStat.exists = true;
+        } else if (currentUserStat.count < 5) {
+            // if count is not reached highest point
+            currentUserStat.count++;
+            if (currentUserStat.timeLimit > block.timestamp) {
+                // if previous time is expired
+                currentUserStat.timeLimit = uint32(
+                    block.timestamp + timeInterval
+                );
+            }
+        } else if (currentUserStat.timeLimit < block.timestamp) {
+            // time is expired
+            // set new time
+            currentUserStat.count = 1;
+            currentUserStat.timeLimit = uint32(block.timestamp + timeInterval);
         } else {
             revert("Reached maximum limit");
         }
+
         _;
     }
 
@@ -48,23 +62,22 @@ contract NFTTESTING is ERC721, Ownable {
     }
 
     function mintNft(address _to, uint256 amount) public {
-        require(amount >= rate, "Amount is not sufficint to mindt nft");
+        require(amount >= rate, "Amount is insifficient.");
 
-        require(whitelisted[msg.sender] == true, "You can't mint this.");
-
+        require(whitelisted[msg.sender] == true, "Invalid access.");
+        ERC20 token = ERC20(tokenAddress);
         uint256 amountToTransfer = amount / rate;
         if (amountToTransfer > 1) {
             for (uint256 i = 0; i < amountToTransfer; i++) {
-                safeMint(_to);
+                safeMint(_to, token);
             }
         } else {
-            safeMint(_to);
+            safeMint(_to, token);
         }
     }
 
-    function safeMint(address to) internal checkUserLimit(to) {
-        IERC20 token = IERC20(tokenAddress);
-        token.transferFrom(msg.sender, address(this), rate);
+    function safeMint(address to, ERC20 _token) internal checkUserLimit(to) {
+        _token.transferFrom(msg.sender, address(this), rate);
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
