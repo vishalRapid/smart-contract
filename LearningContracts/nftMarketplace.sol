75f9a589ff16a96c51491864e0083f8cf9bff4ad
vishal <vishal@vishals-MacBook-Pro.local>
2022-12-15 15:44:58 +0530

    Working on english auction

--- LearningContracts/nftMarketplace.sol
+++ LearningContracts/nftMarketplace.sol
@@ -27,9 +27,41 @@ contract NftMarketplace {
         bool activeListing;
         uint256 price;
         uint256 startTime;
+        uint256 endTime;
     }
 
+    struct AuctionStruct {
+        Auction auctionType;
+        uint256 startDate;
+        uint256 endDate;
+        uint256 startingPrice;
+        uint256 reserverPrice; // for dutch auction
+    }
+
+    // creating struct for offer
+    struct Offer {
+        uint256 nftId;
+        uint256 tokenId;
+        address bidder;
+        uint256 offerPrice;
+    }
+
+    // store nftid along with token details
     mapping(uint256 => Token) Nfts;
+
+    // store offers with mapping to auction
+    mapping(uint256 => Offer[]) nftOffers;
+
+    // setting up event for creating an auction
+    event newAuction(uint256 auctionId, uint256 tokenId, Auction auctionType);
+
+    // setting up event for creating an offer on an auction
+    event newOffer(
+        uint256 auctionId,
+        uint256 indexed tokenId,
+        Auction auctionType,
+        uint256 offerPrice
+    );
 
     // setting up fee for transactions and owner account for deducting fee
     constructor(uint256 _platformFee) {
@@ -60,8 +92,12 @@ contract NftMarketplace {
             Auction.FIXEDPRICE,
             true,
             _price,
+            block.timestamp,
             block.timestamp
         );
+
+        // emit event for newly created auction
+        emit newAuction(items, _tokenId, Auction.FIXEDPRICE);
     }
 
     // buy a fixed price item that is currently on listing
@@ -92,6 +128,96 @@ contract NftMarketplace {
         currentNft.activeListing = false;
     }
 
+    //function to create an auction
+    function createAuction(
+        IERC721 _nft,
+        uint256 _tokenId,
+        AuctionStruct memory _auctionObject
+    ) external {
+        // comparing the owner of the nft
+        require(_nft.ownerOf(_tokenId) == msg.sender, "Invalid request");
+
+        Token memory auctionPayload;
+        if (_auctionObject.auctionType == Auction.FIXEDPRICE) {
+            return;
+        }
+
+        require(
+            _auctionObject.startDate < _auctionObject.endDate,
+            "Invalid start and end date"
+        );
+
+        require(
+            _auctionObject.startDate > block.timestamp,
+            "Start time is invalid"
+        );
+        require(
+            _auctionObject.endDate > block.timestamp,
+            "End time time is invalid"
+        );
+
+        items++;
+        if (_auctionObject.auctionType == Auction.ENGLISH) {
+            // checking validations for english auction
+            auctionPayload = Token(
+                _tokenId,
+                items,
+                _nft,
+                payable(msg.sender),
+                Auction.ENGLISH,
+                true,
+                _auctionObject.startingPrice,
+                _auctionObject.startDate,
+                _auctionObject.endDate
+            );
+        } else {
+            // for dutch auction
+        }
+
+        Nfts[items] = auctionPayload;
+
+        // emit event for newly created auction
+        emit newAuction(items, _tokenId, _auctionObject.auctionType);
+    }
+
+    // create offer to auction
+    function createOffer(uint256 _nftId) external payable {
+        // creating an offer for the auction
+        require(_nftId > 0 && _nftId <= items, "Invalid nft id");
+        Token memory auction = Nfts[_nftId];
+
+        if (auction.listingType == Auction.ENGLISH) {
+            // check if previous offer exist
+            uint256 lastPrice = auction.price;
+            Offer[] memory previousOffer = nftOffers[_nftId];
+            if (previousOffer.length > 0) {
+                //getting latest highest price;
+                lastPrice = previousOffer[previousOffer.length - 1].offerPrice;
+            }
+            // need to check if previous offer exist and then check
+            require(
+                msg.value >= lastPrice,
+                "Bid needs to be greater than last bid"
+            );
+
+            // transfer moeny to contract
+            payable(address(this)).transfer(msg.value);
+
+            //create new offer entry
+            nftOffers[_nftId].push(
+                Offer(_nftId, auction.tokenId, msg.sender, msg.value)
+            );
+        }
+        // creating an event for this auction
+        emit newOffer(_nftId, auction.tokenId, Auction.ENGLISH, msg.value);
+    }
+
+    // function to cancel listing of an nft
+    function cancelListing(uint256 _nftId) external {
+        delete Nfts[_nftId];
+    }
+
+    // function to fetch total price for the nft including platform fee.
     function getTotalPrice(uint256 _itemId) public view returns (uint256) {
         return ((Nfts[_itemId].price * (100 + platformFee)) / 100);
     }
